2b8c17f3ceb66c0a79534d63b0f5ec072c87d812
jyav <133920278+jyav@users.noreply.github.com>
2026-03-02 23:43:41 -0500

    Remove default audio configuration macros
    Removed default audio configuration macros.

--- src/iad/audio/audio_common.h
+++ src/iad/audio/audio_common.h
@@ -3,15 +3,6 @@
 
 #include "cJSON.h"
 
-#define DEFAULT_AI_SAMPLE_RATE AUDIO_SAMPLE_RATE_48000
-#define DEFAULT_AI_CHN_VOL 100
-#define DEFAULT_AI_GAIN 25
-#define DEFAULT_AI_CHN_CNT 1
-#define DEFAULT_AI_FRM_NUM 40
-#define DEFAULT_AI_DEV_ID 0
-#define DEFAULT_AI_CHN_ID 0
-#define DEFAULT_AI_USR_FRM_DEPTH 40
-
 typedef struct {
     cJSON *samplerateItem;
     cJSON *bitwidthItem;
